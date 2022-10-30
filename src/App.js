ecec87bd183e8c062e54bca130783088547f5d24
chungtv <chungred9999@gmail.com>
2022-10-30 12:09:15 +0700

    Add base source code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions/counter";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(10))} style={{marginRight: 10}}>Increase</button>
+      <button onClick={() => dispatch(decrement(10))}>Decrease</button>
+    </div>
+  );
+}
+
+export default App;
